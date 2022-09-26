a09a2b33e53ee1b5b4c1f4162ba03f79b20a53f0
Abraham Chávez Castillo <abraham_cha88@hotmail.com>
2022-09-26 12:08:17 -0500

    Código a revisar en clase

---
+++ Modulo_3/Clase/Ejercicios.py
@@ -0,0 +1,19 @@
+#Escribir una función que reciba otra función y una lista, y devuelva otra lista con el resultado
+# de aplicar la función dada a cada uno de los elementos de la lista.
+
+
+#Escribir una función que reciba una frase y devuelva un diccionario con las palabras que contiene y su longitud.
+
+
+#Una inmobiliaria de una ciudad maneja una lista de inmuebles como la siguiente:
+#[{'año': 2000, 'metros': 100, 'habitaciones': 3, 'garaje': True, 'zona': 'A'},
+#{'año': 2012, 'metros': 60, 'habitaciones': 2, 'garaje': True, 'zona': 'B'},
+#{'año': 1980, 'metros': 120, 'habitaciones': 4, 'garaje': False, 'zona': 'A'},
+#{'año': 2005, 'metros': 75, 'habitaciones': 3, 'garaje': True, 'zona': 'B'},
+#{'año': 2015, 'metros': 90, 'habitaciones': 2, 'garaje': False, 'zona': 'A'}]
+#Construir una función que permita hacer búsqueda de inmuebles en función de un presupuesto dado.
+# La función recibirá como entrada la lista de inmuebles y un precio, y devolverá otra lista con
+# los inmuebles cuyo precio sea menor o igual al proporcionado. Debe hacerlo con clases.
+
+
+#
