b9c8d3c54c67a64d152c29d9dc686d88c81ecbec
Abraham Chávez Castillo <abraham_cha88@hotmail.com>
2022-09-29 20:19:43 -0500

    Update Ejercicio.py

--- Modulo_5/Clase/Ejercicio.py
+++ Modulo_5/Clase/Ejercicio.py
@@ -9,3 +9,11 @@
 #Cuantas propiedades tiene? Las propiedades son los valores separados por ",". Por ejemplo "asd,asd,asd,ad"
 #la linea anterior tienene 4 propiedades
 #Mostrar las letras del alfabeto/dígitos con la cantidad de veces que aparecen en el archivo
+
+#3.- El archivo ApplicationAuditTable.csv tiene texto separado por comas. obtener lo siguiente:
+#Cuántas palabaras tiene todo el archivo y cuántas veces se repiten cada una?
+#Definir un método para encontrar palabras parecidas. Podibles métodos a utilizar:
+# Que coincidan en 2 o más letras
+# Que coincidan en las 2 primeras o las dos últimas letras
+# Que coincidan en las 3 letras de en medio
+#Esto lo que generará son conjuntos de letras parecidas, cada conjunto debe almacenarse en un archivo diferente
